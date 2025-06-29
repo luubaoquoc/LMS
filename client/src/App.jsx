f6fb96e4e52ee6b4f61ec8846a964c96107a9fdf
luubaoquoc <quoclep2003@gmail.com>
2025-06-29 14:58:24 +0700

    HomePage CoursesList CallToAction Companies CourseCard CourseSection footer testimo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
       {!isEducatorRoute && <Navbar />}
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/course-list' element={<CoursesList />} />
+        <Route path='/course-list/:input?' element={<CoursesList />} />
         <Route path='/course/:id' element={<CourseDetails />} />
         <Route path='/my-enrollments' element={<MyEnrollments />} />
         <Route path='/player/:courseId' element={<Player />} />
