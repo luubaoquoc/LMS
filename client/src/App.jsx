ba071b381c236a8c21100bc2b69f38aa00f63237
luubaoquoc <quoclep2003@gmail.com>
2025-08-07 21:23:24 +0700

    AddCourse Mycourse StudentEnrolled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import DashBoard from './pages/educator/DashBoard'
 import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
+import 'quill/dist/quill.snow.css'
 
 
 const App = () => {
