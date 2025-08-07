55f0f483f21cbf160253ec891382c706baee8332
luubaoquoc <quoclep2003@gmail.com>
2025-08-08 00:07:20 +0700

    fix connect DB

--- server/configs/mongoose.js
+++ server/configs/mongoose.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 //connect to the MongoDB database
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URl}/LMS`);
+        await mongoose.connect(`${process.env.MONGODB_URL}/LMS`);
         console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect failure!!!')
