92dda0ee02fe61f63ea384e16b2a58e5f0f16d58
luubaoquoc <quoclep2003@gmail.com>
2025-08-10 23:46:17 +0700

    fix sesions

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -71,7 +71,7 @@ export const stripeWebhooks = async (request, response) => {
             const paymentIntent = event.data.object
             const paymentIntentId = paymentIntent.id
 
-            const session = await stripeInstance.checkout.session.list({
+            const session = await stripeInstance.checkout.sessions.list({
                 payment_intent: paymentIntentId
             })
             const { purchaseId } = session.data[0].metadata
