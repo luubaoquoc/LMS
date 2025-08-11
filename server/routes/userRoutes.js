3a7aeec945d76cfc88e0afd39961f7f00e1242ce
luubaoquoc <quoclep2003@gmail.com>
2025-08-11 20:54:46 +0700

    backend completed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { getUserData, purchaseCourse, userEnrolleredCourses } from '../controllers/userController.js'
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolleredCourses } from '../controllers/userController.js'
+import { useReducer } from 'react'
 
 const userRouter = express.Router()
 
 userRouter.get('/data', getUserData)
 userRouter.get('/enrolled-courses', userEnrolleredCourses)
 userRouter.post('/purchase', purchaseCourse)
+
+userRouter.post('/update-course-progress', updateUserCourseProgress)
+userRouter.get('/get-course-progress', getUserCourseProgress)
+userRouter.post('/add-rating', addUserRating)
 
 export default userRouter
